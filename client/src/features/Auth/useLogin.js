56589112da0da895285e654d0d225a76b8cf0013
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-10-26 17:12:53 +0100

    chore: navigate to homepage on success

--- client/src/features/Auth/useLogin.js
+++ client/src/features/Auth/useLogin.js
@@ -1,13 +1,17 @@
 import { useMutation } from "@tanstack/react-query";
 import { loginApi } from "../../api/apiAuth";
 import toast from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
 
 export const useLogin = () => {
+  const navigate = useNavigate();
+
   const { mutate: login, isLoading } = useMutation({
     mutationFn: loginApi,
 
     onSuccess: () => {
       toast.success("Login successful");
+      navigate("/home");
     },
 
     onError: (error) => {
