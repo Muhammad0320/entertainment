bae4426a309f245de7e460ac4da80bc3e96b4ef0
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-10-26 16:44:38 +0100

    chore: add login api call

--- client/src/api/apiAuth.js
+++ client/src/api/apiAuth.js
@@ -1,1 +1,18 @@
-export const loginApi = () => {};
+import axios from "axios";
+import { BASE_URL } from "../utils/constants";
+
+export const loginApi = ({ email, password }) => {
+  const res = axios({
+    method: "POST",
+    withCredentials: true,
+    url: `${BASE_URL}/users/login`,
+    data: {
+      email,
+      password,
+    },
+  });
+
+  console.log(res.data);
+
+  return res.data;
+};
