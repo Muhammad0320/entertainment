e14dde9122aab324400fd5102d2b88258b831698
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-10-26 19:31:45 +0100

    chore: add signup api axios function

--- client/src/api/apiAuth.js
+++ client/src/api/apiAuth.js
@@ -12,6 +12,21 @@ export const loginApi = async ({ email, password }) => {
     },
   });
 
+  return res.data;
+};
+
+export const signupApi = async ({ email, password, passwordConfirm }) => {
+  const res = await axios({
+    method: "POST",
+    withCredentials: true,
+    url: `${BASE_URL}/users/signup`,
+    data: {
+      email,
+      password,
+      passwordConfirm,
+    },
+  });
+
   console.log(res.data);
 
   return res.data;
