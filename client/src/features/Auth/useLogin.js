84ee659db47b56f5ed94c330dc62567a6e85ca87
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-10-26 17:10:14 +0100

    chore: change the version of react query

--- client/src/features/Auth/useLogin.js
+++ client/src/features/Auth/useLogin.js
@@ -1,1 +1,21 @@
-const useLogin = () => {};
+import { useMutation } from "@tanstack/react-query";
+import { loginApi } from "../../api/apiAuth";
+import toast from "react-hot-toast";
+
+export const useLogin = () => {
+  const { mutate: login, isLoading } = useMutation({
+    mutationFn: loginApi,
+
+    onSuccess: () => {
+      toast.success("Login successful");
+    },
+
+    onError: (error) => {
+      console.log(error);
+
+      toast.error(error.response.data.message);
+    },
+  });
+
+  return { login, isLoading };
+};
