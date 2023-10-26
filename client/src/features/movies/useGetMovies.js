d497be48f2976653942af644bbcdf47086d4c36d
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-10-26 20:37:30 +0100

    chore: add custom hook to get all movies

--- client/src/features/movies/useGetMovies.js
+++ client/src/features/movies/useGetMovies.js
@@ -1,1 +1,12 @@
-export const useGeetMovies = () => {};
+import { useQuery } from "@tanstack/react-query";
+import { getAllMovies } from "../../api/apiMovies";
+
+export const useGetMovies = () => {
+  const { data: allMovies, isLoading } = useQuery({
+    queryFn: getAllMovies,
+
+    queryKey: ["all movies"],
+  });
+
+  return { allMovies, isLoading };
+};
