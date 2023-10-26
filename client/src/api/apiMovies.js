25a75c9a55d3b697408fd37bec234ebe850255d4
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-10-26 20:33:16 +0100

    chore: add axios call to get all movies

--- client/src/api/apiMovies.js
+++ client/src/api/apiMovies.js
@@ -1,1 +1,8 @@
-export const getAllMovies = () => {};
+import axios from "axios";
+import { BASE_URL } from "../utils/constants";
+
+export const getAllMovies = async () => {
+  const res = await axios.get(`${BASE_URL}/movies`, { withCredentials: true });
+
+  return res.data;
+};
