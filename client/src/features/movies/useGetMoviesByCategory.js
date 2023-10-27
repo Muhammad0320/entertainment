ad480b8dea4044dfd091b3b877d80911b0204ace
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-10-27 04:27:22 +0100

    chore: use get movies by categories custom hook is created

--- client/src/features/movies/useGetMoviesByCategory.js
+++ client/src/features/movies/useGetMoviesByCategory.js
@@ -1,1 +1,12 @@
-export const useGetMoviesByCategory = () => {};
+import { useQuery } from "@tanstack/react-query";
+import { getMoviesByCategories } from "../../api/apiMovies";
+
+export const useGetMoviesByCategory = () => {
+  const { data: movieCategory, isLoading } = useQuery({
+    queryFn: getMoviesByCategories,
+
+    queryKey: ["movie-category"],
+  });
+
+  return { movieCategory, isLoading };
+};
