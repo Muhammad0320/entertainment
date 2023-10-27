377e43b931add2b39df69f11f34a3f7ea34d6d83
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-10-27 13:13:04 +0100

    chore: added a custom hook to add item to db bookmarks

--- client/src/features/bookmark/useCreateBookmark.js
+++ client/src/features/bookmark/useCreateBookmark.js
@@ -1,1 +1,19 @@
-const useCreateBookmark = () => {};
+import { useMutation } from "@tanstack/react-query";
+import { createBookmarkApi } from "../../api/apiBookmark";
+import toast from "react-hot-toast";
+
+export const useCreateBookmark = () => {
+  const { mutate: addMovieToBookmark } = useMutation({
+    mutationFn: createBookmarkApi,
+
+    onSuccess: () => {
+      toast.success("successfully added to bookmarks");
+    },
+
+    onError: () => {
+      toast.error(" There was an error while adding to bookmark try again ");
+    },
+  });
+
+  return { addMovieToBookmark };
+};
