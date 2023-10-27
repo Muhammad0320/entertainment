8f944f7261236e44206de9707fb3196608bba908
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-10-27 04:06:43 +0100

    chore: complete the custom hook to get trending movies

--- client/src/features/movies/useGetTrendingMovies.js
+++ client/src/features/movies/useGetTrendingMovies.js
@@ -1,1 +1,11 @@
-const useGetTrendingMovies = () => {};
+import { useQuery } from "@tanstack/react-query";
+import { getTrendingMovies } from "../../api/apiMovies";
+
+export const useGetTrendingMovies = () => {
+  const { data: trendingMovies, isLoading } = useQuery({
+    queryFn: getTrendingMovies,
+    queryKey: ["trending movies"],
+  });
+
+  return { trendingMovies, isLoading };
+};
