3b53a7795f5a5882cee15e444cacba5648c2c50b
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-10-27 05:13:19 +0100

    chore: complete custom hook to get current user

--- client/src/features/user/useGetMe.js
+++ client/src/features/user/useGetMe.js
@@ -1,1 +1,12 @@
-export const useGetMe = () => {};
+import { useQuery } from "@tanstack/react-query";
+import { getMe as getMeApi } from "../../api/apiUser";
+
+export const useGetMe = () => {
+  const { data: me, isLoading } = useQuery({
+    queryFn: getMeApi,
+
+    queryKey: ["me"],
+  });
+
+  return { me, isLoading };
+};
