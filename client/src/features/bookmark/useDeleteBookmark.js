ef84a482e5d12ba60a1b1bb2667f4213fd8eee8c
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-10-27 15:08:17 +0100

    chore: update the custom hook to delete bookmark

--- client/src/features/bookmark/useDeleteBookmark.js
+++ client/src/features/bookmark/useDeleteBookmark.js
@@ -1,1 +1,23 @@
-export const useDeleteBookmark = () => {};
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { deleteBookmarkApi } from "../../api/apiBookmark";
+import toast from "react-hot-toast";
+
+export const useDeleteBookmark = () => {
+  const queryClient = useQueryClient();
+
+  const { mutate: deleteBookmark, isLoading } = useMutation({
+    mutationFn: deleteBookmarkApi,
+
+    onSuccess: () => {
+      toast.success("succesfully deleted bookmark");
+
+      queryClient.invalidateQueries({ queryKey: ["my-bookmarks"] });
+    },
+
+    onError: () => {
+      toast.error("An error occured while deleting your bookmark");
+    },
+  });
+
+  return { deleteBookmark, isLoading };
+};
