b3c5d46240882a818350ed6bea7f300be8476652
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-10-27 05:08:19 +0100

    chore: complete the axios call to get current user

--- client/src/api/apiUser.js
+++ client/src/api/apiUser.js
@@ -1,1 +1,14 @@
-export const getMe = () => {};
+import axios from "axios";
+import { BASE_URL } from "../utils/constants";
+
+export const getMe = async () => {
+  const res = await axios({
+    method: "GET",
+    url: `${BASE_URL}/users/me`,
+
+    withCredentials: true,
+  });
+
+  console.log(res.data);
+  return res.data;
+};
