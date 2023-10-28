bcc5774c4c6b3632b9ea2b6bc0254eb59cec71a4
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-10-28 19:18:54 +0100

    chore: add navbar to app

---
+++ client/src/ui/Navbar.jsx
@@ -0,0 +1,42 @@
+import { styled } from "styled-components";
+import MainNav from "./MainNav";
+import Logo from "./Logo";
+
+import { clampBuilder } from "../Styles/clampBuilder";
+import NavActions from "./NavActions";
+
+const StyledNavBar = styled.aside`
+  text-align: center;
+
+  border-radius: ${() => clampBuilder(950, 1200, 1, 2)};
+
+  background-color: var(--color-blue-dark);
+
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  height: max-content;
+  grid-row: 1 / 2;
+  padding: ${() => clampBuilder(350, 950, 1, 1.5)}
+    ${() => clampBuilder(350, 950, 1.2, 2)};
+
+  @media (max-width: 950px) {
+  }
+
+  @media (max-width: 500px) {
+    margin: 0;
+    border-radius: 0;
+  }
+`;
+
+function NavBar() {
+  return (
+    <StyledNavBar>
+      <Logo />
+      <MainNav />
+      <NavActions />
+    </StyledNavBar>
+  );
+}
+
+export default NavBar;
