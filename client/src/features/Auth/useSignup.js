19e8760c7ae932f79284cd4bf54f005265931149
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-11-17 10:52:57 +0100

    chore: restructured imports of use signup comp

--- client/src/features/Auth/useSignup.js
+++ client/src/features/Auth/useSignup.js
@@ -1,7 +1,7 @@
-import { useMutation } from "@tanstack/react-query";
-import { signupApi } from "../../api/apiAuth";
 import toast from "react-hot-toast";
+import { signupApi } from "../../api/apiAuth";
 import { useNavigate } from "react-router-dom";
+import { useMutation } from "@tanstack/react-query";
 
 export const useSignup = () => {
   const navigate = useNavigate();
