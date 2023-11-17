88a980a18c37f0fe4143f8d7a875f30c6aa207b8
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-11-17 10:15:11 +0100

    chore: update the protedroutes comp

--- client/src/features/Auth/ProtectedRoutes.jsx
+++ client/src/features/Auth/ProtectedRoutes.jsx
@@ -3,7 +3,7 @@ import { Navigate } from "react-router-dom";
 function ProtectedRoutes({ children }) {
   const user = JSON.parse(localStorage.getItem("user"));
 
-  return user ? children : <Navigate to="/login" />;
+  return user ? children : <Navigate to="/login" replace />;
 }
 
 export default ProtectedRoutes;
