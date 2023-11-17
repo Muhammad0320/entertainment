101a17e6a75b28f8daf72f921dfc6f3e522f6c2c
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-11-17 09:55:03 +0100

    chore: cors

--- server/app.js
+++ server/app.js
@@ -34,7 +34,7 @@ app.use(sanitize());
 app.use(xss());
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://127.0.0.1:5174");
+  res.header("Access-Control-Allow-Origin", "http://127.0.0.1:5173");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Headers, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization"
@@ -49,7 +49,7 @@ app.use(function(req, res, next) {
 app.use(
   cors({
     origin: [
-      "http://127.0.0.1:5174",
+      "http://127.0.0.1:5173",
       "https://entertainment-app-muhammad0320.vercel.app"
     ],
     methods: ["POST", "PUT", "GET", "OPTIONS", "HEAD", "PATCH", "DELETE"],
